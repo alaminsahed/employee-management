72fa727bc214db4568cd5b9f753df27b2ae23ab8
alaminsahed <alaminsahed97@gmail.com>
2023-06-09 16:34:45 +0600

    styles: my profile home page

--- frontend/src/pages/shared/profile/MyProfile.tsx
+++ frontend/src/pages/shared/profile/MyProfile.tsx
@@ -3,16 +3,18 @@ import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 
-import { Box, Card, Typography, Button } from "@mui/material";
+import { Box, Card, Typography, Button, Paper } from "@mui/material";
 
 
 import { isLogin } from "../../../utils/auth";
+import { getDevice } from "../../../utils/getScreenSize";
 import { toast } from "react-toastify";
 
 
 const MyProfile = () => {
   const navigate = useNavigate();
   const [profile, setProfile] = React.useState < any > ({});
+
 
 
   const params = useParams();
@@ -23,6 +25,7 @@ const MyProfile = () => {
       navigate("/");
     }
     profileId ? profileDetails() : fecthData();
+
   }, []);
 
   const profileDetails = async () => {
@@ -70,9 +73,18 @@ const MyProfile = () => {
     }
   };
 
-
   return (
     <div>
+      {profileId ? (
+        <Typography variant="h3" sx={{ textAlign: "center", padding: 2, fontFamily: "cursive", xs: { fontSize: "1rem" } }}>
+          Profile Details
+        </Typography>
+      ) : (
+        <Typography variant="h3" sx={{ textAlign: "center", padding: 2, fontFamily: "monospace", fontSize: { xs: "1rem", md: "3rem", sm: "2rem", } }}>
+          My Profile
+        </Typography>
+      )}
+
       <Box
         sx={{
           display: "flex",
@@ -80,23 +92,14 @@ const MyProfile = () => {
           justifyContent: "space-around",
         }}
       >
-
         {profileId ? (
-          <Typography variant="h3" sx={{ textAlign: "center", padding: 2, fontFamily: "cursive" }}>
-            Profile Details
-          </Typography>
-        ) : (
-          <Typography variant="h3" sx={{ textAlign: "center", padding: 2, fontFamily: "cursive" }}>
-            My Profile
-          </Typography>
-        )}
-        {profileId ? (
-          <div>
+          <>
             <Button
               color="secondary"
               variant="outlined"
               sx={{ height: "3rem", p: 2, mt: 2, color: "white", mr: 2 }}
               onClick={() => navigate(`/addEmployee/${profileId}`)}
+              className="animate__animated animate__bounce animate__delay-2s"
             >
               Edit
             </Button>
@@ -109,46 +112,51 @@ const MyProfile = () => {
             >
               Delete
             </Button>
-          </div>
+          </>
         ) : (
           <>
             <Button
               color="primary"
               variant="outlined"
-              sx={{ height: "3rem", p: 2, mt: 2 }}
+              sx={{ height: "3rem", p: 2, m: 1 }}
               onClick={() => navigate("/changePassword")}
+              className="animate__animated animate__lightSpeedInRight animate__delay-4s"
             >
               Change Password
             </Button>
             <Button
               color="secondary"
               variant="outlined"
-              sx={{ height: "3rem", p: 2, mt: 2 }}
+              sx={{ height: "3rem", p: 2, m: 1 }}
               onClick={() => navigate("/profileEdit")}
+              className="animate__animated animate__lightSpeedInRight animate__delay-3s"
             >
               Profile Edit
             </Button>
             <Button
               color="success"
               variant="outlined"
-              sx={{ height: "3rem", p: 2, mt: 2 }}
+              sx={{ height: "3rem", p: 2, m: 1 }}
               onClick={() => navigate(`/claimLeaveRequest/${profile._id}`)}
+              className="animate__animated animate__lightSpeedInRight animate__delay-2s"
             >
-              Claim Leave Request
+              Claim Leave
             </Button>
             <Button
               color="info"
               variant="outlined"
-              sx={{ height: "3rem", p: 2, mt: 2 }}
+              sx={{ height: "3rem", p: 2, m: 1 }}
               onClick={() => navigate("/leaveRequestStatus")}
+              className="animate__animated animate__lightSpeedInRight animate__delay-1s"
             >
-              Leave Request Status
+              Leaves Status
             </Button>
             <Button
               color="primary"
               variant="outlined"
-              sx={{ height: "3rem", p: 2, mt: 2 }}
+              sx={{ height: "3rem", p: 2, m: 1 }}
               onClick={() => navigate("/payslip")}
+              className="animate__animated animate__lightSpeedInRight"
             >
               Download Pay Slip
             </Button>
@@ -156,105 +164,102 @@ const MyProfile = () => {
         )}
 
       </Box>
-      <div>
-        <Box>
-          <Box sx={{ display: { md: "flex" } }}>
-            <Card
-              sx={{
-                padding: 5,
-                margin: 2,
-                width: { md: "40rem" },
-                boxShadow: "md",
-                translate: "translateY(50%)",
-                bgcolor: "#526b59",
-                color: "white",
-              }}
-              className="animate__animated animate__fadeInDownBig"
-            >
-              <Typography
-                variant="h4"
-                sx={{
-                  textAlign: "center",
-                  pb: 5,
-                  fontWeight: "bold",
-                  color: "white",
-                }}
-              >
-                Personal Information
-              </Typography>
-              <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-                <div>
-                  <p>Name: {profile?.name}</p>
-                  <p>Role: {profile?.role}</p>
-                  <p>Email: {profile?.email}</p>
-                  <p>Phone: {profile?.phone}</p>
-                  <p>Designation: {profile?.designation}</p>
-                  <p>Employee Status: {profile?.employeeStatus}</p>
-                </div>
-                <div>
-                  <p>Address: {profile?.address}</p>
-                  <p>Salary: {profile?.salary}</p>
-                  <p>Joinning Date: {profile?.joingDate}</p>
-                  <p>image</p>
-                  <img
-                    src={`http://localhost:5000${profile?.image}`}
-                    alt=""
-                    style={{ height: "10rem", width: "10rem" }}
-                  />
-                </div>
-              </Box>
-            </Card>
-            <Card
-              sx={{
-                padding: 5,
-                margin: 2,
-                width: { md: "40rem" },
-                boxShadow: "md",
-                translate: "translateY(50%)",
-                bgcolor: "#bf6c5e",
-                color: "#0d0201",
-              }}
-              className="animate__animated animate__lightSpeedInRight"
-            >
-              <div>
-                <Typography
-                  variant="h4"
-                  sx={{ textAlign: "center", pb: 5, fontWeight: "bold" }}
-                >
-                  Project Information
-                </Typography>
-                <p>Project Name: {profile?.currentProjects?.projectName}</p>
-                <p>Resposiblity: {profile?.currentProjects?.responsiblity}</p>
-                <p>Staus: {profile?.currentProjects?.status}</p>
-              </div>
-            </Card>
-          </Box>
-          <Card
+      <Box sx={{ display: { md: "flex" }, flexDirection: { md: "row" }, justifyContent: "center", p: { md: 5 } }}>
+        <Card
+          sx={{
+            padding: 5,
+            margin: 2,
+            width: { md: "40rem" },
+            boxShadow: "md",
+            translate: "translateY(50%)",
+            bgcolor: "#526b59",
+            color: "white",
+          }}
+          className="animate__animated animate__fadeInDownBig"
+        >
+          <Typography
+            variant="h4"
             sx={{
-              padding: 5,
-              margin: 2,
-              width: { md: "40rem" },
-              boxShadow: "md",
-              translate: "translateY(50%)",
-              bgcolor: "#11465c",
+              textAlign: "center",
+              pb: 5,
+              fontWeight: "bold",
               color: "white",
             }}
-            className="animate__animated animate__slideInUp"
           >
+            Personal Information
+          </Typography>
+          <Box sx={{ display: "flex", justifyContent: "space-between" }}>
             <div>
-              <Typography
-                variant="h4"
-                sx={{ textAlign: "center", pb: 5, fontWeight: "bold" }}
-              >
-                Performance Information
-              </Typography>
-              <p>Bonus: {profile?.professionalInfo?.bonus}</p>
-              <p>Total Leaves: {profile?.professionalInfo?.totalLeave}</p>
+              <p>Name: {profile?.name}</p>
+              <p>Role: {profile?.role}</p>
+              <p>Email: {profile?.email}</p>
+              <p>Phone: {profile?.phone}</p>
+              <p>Designation: {profile?.designation}</p>
+              <p>Employee Status: {profile?.employeeStatus}</p>
             </div>
-          </Card>
-        </Box>
-      </div>
-    </div>
+            <div>
+              <p>Address: {profile?.address}</p>
+              <p>Salary: {profile?.salary}</p>
+              <p>Joinning Date: {profile?.joingDate}</p>
+              <p>image</p>
+              <img
+                src={`http://localhost:5000${profile?.image}`}
+                alt=""
+                style={{ height: "10rem", width: "10rem" }}
+              />
+            </div>
+          </Box>
+        </Card>
+        <Card
+          sx={{
+            padding: 5,
+            margin: 2,
+            width: { md: "40rem" },
+            boxShadow: "md",
+            translate: "translateY(50%)",
+            bgcolor: "#bf6c5e",
+            color: "#0d0201",
+          }}
+          className="animate__animated animate__lightSpeedInRight"
+        >
+          <div>
+            <Typography
+              variant="h4"
+              sx={{ textAlign: "center", pb: 5, fontWeight: "bold" }}
+            >
+              Project Information
+            </Typography>
+            <p>Project Name: {profile?.currentProjects?.projectName}</p>
+            <p>Resposiblity: {profile?.currentProjects?.responsiblity}</p>
+            <p>Staus: {profile?.currentProjects?.status}</p>
+          </div>
+        </Card>
+        <Card
+          sx={{
+            padding: 5,
+            margin: 2,
+            width: { md: "40rem" },
+            boxShadow: "md",
+            translate: "translateY(50%)",
+            bgcolor: "#11465c",
+            color: "white",
+          }}
+          className="animate__animated animate__slideInUp"
+        >
+          <div>
+            <Typography
+              variant="h4"
+              sx={{ textAlign: "center", pb: 5, fontWeight: "bold" }}
+            >
+              Performance Information
+            </Typography>
+            <p>Bonus: {profile?.professionalInfo?.bonus}</p>
+            <p>Total Leaves: {profile?.professionalInfo?.totalLeave}</p>
+          </div>
+        </Card>
+      </Box>
+
+    </div >
   );
 };
 
